6fac74c0f363880c4c23c73dc1ad117d0dafeb76
citadelstech-it <krishnaguduri08@gmail.com>
2026-03-02 19:07:46 +0530

    Production DB config

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,21 +1,36 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     port: process.env.DB_PORT,
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false, // For self-signed certificates
+//       },
+//     },
+//   }
+// );
+
+// module.exports = sequelize;
+
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // For self-signed certificates
-      },
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // For self-signed certificates
     },
-  }
-);
+  },
+});
 
 module.exports = sequelize;
